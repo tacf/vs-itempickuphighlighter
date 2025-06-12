c0e67e2e36ff8a34768608f8625da61b86b14879
Tiago Ferreira <dev.tiago@gmail.com>
2025-06-12 15:15:24 +0100

    feat: initial simple particle implementation

---
+++ ItemPickupHighlighter/ItemPickupHighlighterModSystem.cs
@@ -0,0 +1,49 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace ItemPickupHighlighter;
+
+public class ItemPickupHighlighterModSystem : ModSystem
+{
+    private ICoreClientAPI _capi;
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
+    private readonly int _highlightRange = 10;
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        _capi = api;
+        _capi.Input.RegisterHotKey(Constants.HotKeyIdentifier, Constants.HotKeyLabel, GlKeys.F, shiftPressed: true);
+        _capi.Input.SetHotKeyHandler(Constants.HotKeyIdentifier, HighlightItems);
+        
+        base.StartClientSide(_capi);
+    }
+
+    private bool HighlightItems(KeyCombination hotkey)
+    {
+        var et = _capi.World.Player.Entity.Api.World.GetEntitiesAround(_capi.World.Player.Entity.SidedPos.XYZ,
+            _highlightRange,
+            _highlightRange);
+        foreach (var ent in et)
+        {
+            if (ent is EntityItem)
+            {
+                _capi.World.SpawnParticles(new SimpleParticleProperties()
+                {
+                    MinPos = ent.SidedPos.XYZ,
+                    Color = ColorUtil.WhiteArgb,
+                    MinSize = 0.1f,
+                    MaxSize = 0.1f,
+                    MinVelocity = new Vec3f(-0.1f, 0.5f, -0.1f),
+                    AddVelocity = new Vec3f(0.1f, 1.5f, 0.1f),
+                    MinQuantity = 0.7f,
+                    LifeLength = 1,
+                    WithTerrainCollision = false,
+                    LightEmission = ColorUtil.WhiteArgb
+                });
+            }
+        }
+
+        return true;
+    }
+}
---
+++ ItemPickupHighlighter/utils/Constants.cs
@@ -0,0 +1,9 @@
+using Vintagestory.API.Config;
+
+namespace ItemPickupHighlighter;
+
+public static class Constants
+{
+    public const string HotKeyIdentifier = "highlightitems";
+    public const string HotKeyLabel = "Highlight Ground Items";
+}
